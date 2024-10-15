a34378dce7004145e488edf39e07a9a16116adf2
Baksteennz <christoeras@proton.me>
2024-10-15 15:32:25 +1300

    M

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,11 @@ local keymap = vim.keymap
 
 --custom keys
 vim.g.mapleader = " "
+keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 --vim settings
 opt.number = true
@@ -11,7 +16,9 @@ opt.relativenumber = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-opt.autoindent =true
+opt.autoindent = true
+opt.smartindent = true
 opt.termguicolors = true
 opt.clipboard:append("unnamedplus")
-
+opt.ignorecase = true
+opt.smartcase = true

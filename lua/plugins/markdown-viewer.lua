ea1928ceafaea1db87fbe8abf0837e947afcd71c
Baksteennz <43923125+loaDer1232@users.noreply.github.com>
2025-03-02 23:35:56 +1300

    Update markdown-viewer.lua

--- lua/plugins/markdown-viewer.lua
+++ lua/plugins/markdown-viewer.lua
@@ -4,4 +4,95 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+    require('render-markdown').setup({
+    link = {
+        -- Turn on / off inline link icon rendering
+        enabled = true,
+        -- Additional modes to render links
+        render_modes = false,
+        -- How to handle footnote links, start with a '^'
+        footnote = {
+            -- Replace value with superscript equivalent
+            superscript = true,
+            -- Added before link content when converting to superscript
+            prefix = '',
+            -- Added after link content when converting to superscript
+            suffix = '',
+        },
+        -- Inlined with 'image' elements
+        image = '󰥶 ',
+        -- Inlined with 'email_autolink' elements
+        email = '󰀓 ',
+        -- Fallback icon for 'inline_link' and 'uri_autolink' elements
+        hyperlink = '󰌹 ',
+        -- Applies to the inlined icon as a fallback
+        highlight = 'RenderMarkdownLink',
+        -- Applies to WikiLink elements
+        wiki = {
+            icon = '󱗖 ',
+            body = function()
+                return nil
+            end,
+            highlight = 'RenderMarkdownWikiLink',
+        },
+        -- Define custom destination patterns so icons can quickly inform you of what a link
+        -- contains. Applies to 'inline_link', 'uri_autolink', and wikilink nodes. When multiple
+        -- patterns match a link the one with the longer pattern is used.
+        -- Can specify as many additional values as you like following the 'web' pattern below
+        --   The key in this case 'web' is for healthcheck and to allow users to change its values
+        --   'pattern':   Matched against the destination text see :h lua-pattern
+        --   'icon':      Gets inlined before the link text
+        --   'highlight': Optional highlight for the 'icon', uses fallback highlight if not provided
+        custom = {
+            web = { pattern = '^http', icon = '󰖟 ' },
+            discord = { pattern = 'discord%.com', icon = '󰙯 ' },
+            github = { pattern = 'github%.com', icon = '󰊤 ' },
+            gitlab = { pattern = 'gitlab%.com', icon = '󰮠 ' },
+            google = { pattern = 'google%.com', icon = '󰊭 ' },
+            neovim = { pattern = 'neovim%.io', icon = ' ' },
+            reddit = { pattern = 'reddit%.com', icon = '󰑍 ' },
+            stackoverflow = { pattern = 'stackoverflow%.com', icon = '󰓌 ' },
+            wikipedia = { pattern = 'wikipedia%.org', icon = '󰖬 ' },
+            youtube = { pattern = 'youtube%.com', icon = '󰗃 ' },
+        },
+                -- Checkboxes are a special instance of a 'list_item' that start with a 'shortcut_link'
+    -- There are two special states for unchecked & checked defined in the markdown grammar
+    checkbox = {
+        -- Turn on / off checkbox state rendering
+        enabled = true,
+        -- Additional modes to render checkboxes
+        render_modes = false,
+        -- Determines how icons fill the available space:
+        --  inline:  underlying text is concealed resulting in a left aligned icon
+        --  overlay: result is left padded with spaces to hide any additional text
+        position = 'inline',
+        unchecked = {
+            -- Replaces '[ ]' of 'task_list_marker_unchecked'
+            icon = '󰄱 ',
+            -- Highlight for the unchecked icon
+            highlight = 'RenderMarkdownUnchecked',
+            -- Highlight for item associated with unchecked checkbox
+            scope_highlight = nil,
+        },
+        checked = {
+            -- Replaces '[x]' of 'task_list_marker_checked'
+            icon = '󰱒 ',
+            -- Highlight for the checked icon
+            highlight = 'RenderMarkdownChecked',
+            -- Highlight for item associated with checked checkbox
+            scope_highlight = nil,
+        },
+        -- Define custom checkbox states, more involved as they are not part of the markdown grammar
+        -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks
+        -- Can specify as many additional states as you like following the 'todo' pattern below
+        --   The key in this case 'todo' is for healthcheck and to allow users to change its values
+        --   'raw':             Matched against the raw text of a 'shortcut_link'
+        --   'rendered':        Replaces the 'raw' value when rendering
+        --   'highlight':       Highlight for the 'rendered' icon
+        --   'scope_highlight': Highlight for item associated with custom checkbox
+        custom = {
+            todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+        },
+    },
+})
 }
